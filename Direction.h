8bdceb47622994369d3acdf11ab3ca04a3ca391f
Wahid Tanner <wahidtanner@gmail.com>
2016-09-05 16:13:14 -0400

    Added Direction enum.

---
+++ Direction.h
@@ -0,0 +1,23 @@
+//
+//  Direction.h
+//  Bounce
+//
+//  Created by Abdul Wahid Tanner on 9/5/16.
+//  Copyright © 2016 Take Up Code. All rights reserved.
+//
+
+#pragma once
+
+enum class Direction
+{
+    East = 0,
+    West,
+    Up,
+    Down,
+    North,
+    South,
+    NorthEast,
+    NorthWest,
+    SouthEast,
+    SouthWest
+};
